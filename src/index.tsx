0cabd03bd5aa7ad29dce6c873f8a3d41cfcdf910
Oliver Schweikert <oliver.schweikert@pm.me>
2022-08-03 18:08:47 +1200

    Initial Commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import { ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
 import * as ReactDOM from "react-dom/client"
 import { App } from "./App"
-import reportWebVitals from "./reportWebVitals"
-import * as serviceWorker from "./serviceWorker"
+import reportWebVitals from "./services/reportWebVitals"
+import * as serviceWorker from "./services/serviceWorker"
 
 
 const container = document.getElementById("root")
